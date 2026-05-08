20bbdcebe91c1fc413bfc7cb7e6c9629e9bf0f98
dex0o0 <hassan13854444@gmail.com>
2026-05-08 17:25:49 +0330

    chenge string input rds to pathbuf

--- src/commands/module/readstory.rs
+++ src/commands/module/readstory.rs
@@ -1,35 +1,31 @@
 use super::smalmodule::sleep;
-use crossterm::{style::Stylize};
-use std::{
-    fs, 
-    process::Command
-};
+use crossterm::style::Stylize;
+use std::{fs, path::PathBuf, process::Command};
 
-pub fn read_file_story(path:String){
+pub fn read_file_story(path: PathBuf) {
     let _ = Command::new("clear").spawn();
     sleep(1000);
-    let content = fs::read_to_string(&path)
-        .expect("Error to read file?!");
-    let mut count:u8 = 0;
-    let mut rang:char = ' ';
-    for line in content.lines(){
-        for char in line.chars(){
+    let content = fs::read_to_string(&path).expect("Error to read file?!");
+    let mut count: u8 = 0;
+    let mut rang: char = ' ';
+    for line in content.lines() {
+        for char in line.chars() {
             if count == 3 {
                 count = 0;
-            }; 
-            if char=='_'{
+            };
+            if char == '_' {
                 sleep(500);
                 eprint!(" ");
                 continue;
-            }else if char==' ' {
+            } else if char == ' ' {
                 sleep(80);
-                eprint!("{}",char); 
+                eprint!("{}", char);
                 continue;
-            }else if char=='\\' {
+            } else if char == '\\' {
                 if count != 3 {
-                    count += 1;  
+                    count += 1;
                     continue;
-                }else{
+                } else {
                     count = 0;
                     continue;
                 }
@@ -37,21 +33,20 @@ pub fn read_file_story(path:String){
             if count == 1 {
                 rang = char;
                 continue;
-            }else if count == 2 {
+            } else if count == 2 {
                 match &rang {
-                    'r' => eprint!("{}",char.red()),
-                    'b' => eprint!("{}",char.blue()),
-                    'y' => eprint!("{}",char.yellow()),
-                    'g' => eprint!("{}",char.green()),
-                    'B'   => eprint!("{}",char.bold()),
-                    _ => {},
+                    'r' => eprint!("{}", char.red()),
+                    'b' => eprint!("{}", char.blue()),
+                    'y' => eprint!("{}", char.yellow()),
+                    'g' => eprint!("{}", char.green()),
+                    'B' => eprint!("{}", char.bold()),
+                    _ => {}
                 }
                 continue;
             }
-            
-            eprint!("{}",char);
-            sleep(80);
 
+            eprint!("{}", char);
+            sleep(80);
         }
         println!();
     }
